742e93ac711c0e4b85637a9574df303adb5f3d7e
Brian Lerner <bnlerner@gmail.com>
2025-06-07 23:06:46 -0400

    adds marlin and other config

--- compile_upload_test.sh
+++ compile_upload_test.sh
@@ -8,7 +8,13 @@ SERIAL_PORT=/dev/ttyACM0
 # Set the baud rate
 BAUD_RATE=115200
 
+# arduino-cli compile --fqbn arduino:avr:mega Marlin/Marlin.ino
+# arduino-cli upload -p $SERIAL_PORT --fqbn arduino:avr:mega Marlin/Marlin.ino
+
+arduino-cli compile --fqbn arduino:avr:mega ZYLtechHC/ZYLtechHC.ino
+arduino-cli upload -p $SERIAL_PORT --fqbn arduino:avr:mega ZYLtechHC/ZYLtechHC.ino
+
 # Set the test steps
-arduino-cli compile --fqbn arduino:avr:mega $1
-arduino-cli upload -p $SERIAL_PORT --fqbn arduino:avr:mega $1
+# arduino-cli compile --fqbn arduino:avr:mega $1
+# arduino-cli upload -p $SERIAL_PORT --fqbn arduino:avr:mega $1
 arduino-cli monitor -p $SERIAL_PORT -c baudrate=$BAUD_RATE
