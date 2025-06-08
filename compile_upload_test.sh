02fe89eba015c5cb5736da0c12d74486327fc7dd
Brian Lerner <bnlerner@gmail.com>
2025-06-07 21:17:46 -0400

    adds drivers

---
+++ compile_upload_test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# This script is used to test the RAMPS pins on the Zyltech HC board
+
+# Set the serial port
+SERIAL_PORT=/dev/ttyACM0
+
+# Set the baud rate
+BAUD_RATE=115200
+
+# Set the test steps
+arduino-cli compile --fqbn arduino:avr:mega $1
+arduino-cli upload -p $SERIAL_PORT --fqbn arduino:avr:mega $1
+arduino-cli monitor -p $SERIAL_PORT -c baudrate=$BAUD_RATE
